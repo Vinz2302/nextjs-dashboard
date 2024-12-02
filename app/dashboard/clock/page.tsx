8fa0db73641d666049e188e9198b3fd8c5a40a94
Vinz <vincentkosasih2302@gmail.com>
2024-12-02 14:15:04 +0700

    command testing

--- app/dashboard/clock/page.tsx
+++ app/dashboard/clock/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     return (
         // <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <div className='flex items-center justify-center h-screen overflow-hidden bg-white'>
-            <h1>Testing</h1>
+            {/* <h1>Testing</h1> */}
             <Clock/> {/* Size is optional; it will resize responsively */}
         </div>
     )
